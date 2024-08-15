372047d7bacc56983486822943ac8f30fbb9610d
FelipeAssayag <felipe.ma8@puccampinas.edu.br>
2024-08-15 16:39:50 -0300

    Update Tarefa002_1.cpp

--- Exercicios/Tarefa002_1.cpp
+++ Exercicios/Tarefa002_1.cpp
@@ -1,13 +1,11 @@
-//Código de equação da reta
-//Importar bibliotecas
 #include<iostream>
 using namespace std;
-//Iniciar código principal
+
 int main()
 {
-    //Criação de variáveis
+    //variáveis
     float x, y, z, w;
-    //Entrada de dados pelos usuários
+    //Numero de visitantes
     cout << "\nInsira o numero de visitantes da primeira semana :";
     cin >> x;
     cout << "\nInsira o numero de visitantes da segunda semana :";
@@ -16,7 +14,7 @@ int main()
     cin >> z;
     cout << "\nInsira o numero de visitantes da quarta semana :";
     cin >> w;
-    //Processos
+    //Calculo
     float R = x + y + z + w;
     //Saída
     cout << "\nNeste mes foram recebidos " << R << " visitantes.";
